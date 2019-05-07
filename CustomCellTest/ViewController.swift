bfdbb62a17b3913052f1945a6b40486c37a11fb6
Satoru_Aikawa <other_usage@icloud.com>
2019-05-07 21:01:52 +0900

    Initial Commit

---
+++ CustomCellTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCellTest
+//
+//  Created by USER on 2019/05/07.
+//  Copyright © 2019 SoLaMi Smile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
